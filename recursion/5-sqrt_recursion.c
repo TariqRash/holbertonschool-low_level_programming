d90dc18e95199bc71c812c121d3ac5385aa27d28
root <root@ip-10-42-46-13.ec2.internal>
2025-09-10 06:43:08 +0000

    Add recursion project with all recursive functions

---
+++ recursion/5-sqrt_recursion.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * sqrt_helper - helper function for _sqrt_recursion
+ * @n: number to find square root of
+ * @guess: current guess
+ *
+ * Return: square root or -1
+ */
+int sqrt_helper(int n, int guess)
+{
+	if (guess * guess == n)
+		return (guess);
+	if (guess * guess > n)
+		return (-1);
+	return (sqrt_helper(n, guess + 1));
+}
+
+/**
+ * _sqrt_recursion - returns the natural square root of a number
+ * @n: number to find square root of
+ *
+ * Return: natural square root, or -1 if no natural square root
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	if (n == 0 || n == 1)
+		return (n);
+	return (sqrt_helper(n, 1));
+}
---
+++ recursion/README.md
@@ -0,0 +1,17 @@
+# C - Recursion
+
+This project covers:
+- Understanding and implementing recursion
+- When to use recursion vs loops
+- Recursive algorithms for common problems
+
+## Files
+- 0-puts_recursion.c - prints string with newline
+- 1-print_rev_recursion.c - prints string in reverse
+- 2-strlen_recursion.c - calculates string length
+- 3-factorial.c - calculates factorial
+- 4-pow_recursion.c - calculates power
+- 5-sqrt_recursion.c - finds square root
+- 6-is_prime_number.c - checks if number is prime
+- 100-is_palindrome.c - checks if string is palindrome
+- 101-wildcmp.c - compares strings with wildcards
