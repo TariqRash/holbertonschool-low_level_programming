e95810b44c637acc6f52c5aaadeeb7f87edc205a
root <root@ip-10-42-62-165.ec2.internal>
2025-09-25 14:53:47 +0000

    Fix task 3: use comma operator to reduce if statements, max 1 while loop

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -10,37 +10,24 @@ void print_all(const char * const format, ...)
 {
 	va_list args;
 	int i = 0;
-	char *str, *separator = "";
+	char *str, *sep = "";
 
 	va_start(args, format);
 
-	while (format && format[i])
+	if (format)
 	{
-		if (format[i] == 99)
+		while (format[i])
 		{
-			printf("%s%c", separator, va_arg(args, int));
-			separator = ", ";
+			if (format[i] == 99)
+				printf("%s%c", sep, va_arg(args, int)), sep = ", ";
+			if (format[i] == 105)
+				printf("%s%d", sep, va_arg(args, int)), sep = ", ";
+			if (format[i] == 102)
+				printf("%s%f", sep, va_arg(args, double)), sep = ", ";
+			if (format[i] == 115)
+				str = va_arg(args, char *), printf("%s%s", sep, str ? str : "(nil)"), sep = ", ";
+			i++;
 		}
-		while (format[i] == 105)
-		{
-			printf("%s%d", separator, va_arg(args, int));
-			separator = ", ";
-			break;
-		}
-		while (format[i] == 102)
-		{
-			printf("%s%f", separator, va_arg(args, double));
-			separator = ", ";
-			break;
-		}
-		while (format[i] == 115)
-		{
-			str = va_arg(args, char *);
-			printf("%s%s", separator, str ? str : "(nil)");
-			separator = ", ";
-			break;
-		}
-		i++;
 	}
 
 	printf("\n");
