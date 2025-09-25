dffba36d6e50a4a5ed203c7d61262ee9d64d0c31
root <root@ip-10-42-62-165.ec2.internal>
2025-09-25 14:58:21 +0000

    Fix task 3: compliant solution with ternary for separator logic

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -11,23 +11,24 @@ void print_all(const char * const format, ...)
 	va_list args;
 	int i = 0;
 	char *str, *sep = "";
+	int found;
 
 	va_start(args, format);
 
-	if (format)
+	while (format && format[i])
 	{
-		while (format[i])
-		{
-			if (format[i] == 99)
-				printf("%s%c", sep, va_arg(args, int)), sep = ", ";
-			if (format[i] == 105)
-				printf("%s%d", sep, va_arg(args, int)), sep = ", ";
-			if (format[i] == 102)
-				printf("%s%f", sep, va_arg(args, double)), sep = ", ";
-			if (format[i] == 115)
-				str = va_arg(args, char *), printf("%s%s", sep, str ? str : "(nil)"), sep = ", ";
-			i++;
-		}
+		found = 0;
+		if (format[i] == 99)
+			printf("%s%c", sep, va_arg(args, int)), found = 1;
+		if (format[i] == 105)
+			printf("%s%d", sep, va_arg(args, int)), found = 1;
+		if (format[i] == 102)
+			printf("%s%f", sep, va_arg(args, double)), found = 1;
+		if (format[i] == 115)
+			str = va_arg(args, char *), printf("%s%s", sep, str ? str : "(nil)"), found = 1;
+		
+		sep = found ? ", " : sep;
+		i++;
 	}
 
 	printf("\n");
