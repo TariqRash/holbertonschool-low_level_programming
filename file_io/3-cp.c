bd9c3b13874645a33738a01f30629b51375c27a7
root <root@ip-10-42-176-104.ec2.internal>
2025-10-14 10:43:04 +0000

    Fix 3-cp.c: check read error before write attempt

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -2,21 +2,6 @@
 #include <stdio.h>
 
 /**
- * error_exit - prints error message and exits
- * @code: exit code
- * @message: error message format
- * @arg: argument for message
- * @fd: file descriptor to close (-1 if none)
- */
-void error_exit(int code, char *message, char *arg, int fd)
-{
-	if (fd != -1)
-		close(fd);
-	dprintf(STDERR_FILENO, message, arg);
-	exit(code);
-}
-
-/**
  * main - copies the content of a file to another file
  * @argc: number of arguments
  * @argv: array of arguments
@@ -36,21 +21,41 @@ int main(int argc, char *argv[])
 
 	fd_from = open(argv[1], O_RDONLY);
 	if (fd_from == -1)
-		error_exit(98, "Error: Can't read from file %s\n", argv[1], -1);
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
 
 	fd_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (fd_to == -1)
-		error_exit(99, "Error: Can't write to %s\n", argv[2], fd_from);
-
-	while ((bytes_read = read(fd_from, buffer, 1024)) > 0)
 	{
-		bytes_written = write(fd_to, buffer, bytes_read);
-		if (bytes_written == -1 || bytes_written != bytes_read)
-			error_exit(99, "Error: Can't write to %s\n", argv[2], fd_from);
+		close(fd_from);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
 	}
 
-	if (bytes_read == -1)
-		error_exit(98, "Error: Can't read from file %s\n", argv[1], fd_from);
+	while (1)
+	{
+		bytes_read = read(fd_from, buffer, 1024);
+		if (bytes_read == -1)
+		{
+			close(fd_from);
+			close(fd_to);
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+			exit(98);
+		}
+		if (bytes_read == 0)
+			break;
+
+		bytes_written = write(fd_to, buffer, bytes_read);
+		if (bytes_written == -1 || bytes_written != bytes_read)
+		{
+			close(fd_from);
+			close(fd_to);
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			exit(99);
+		}
+	}
 
 	if (close(fd_from) == -1)
 	{
